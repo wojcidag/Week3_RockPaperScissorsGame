fbbf37dbf96c43ddd3dfddc1da0ae16277bbfe7d
Dagomir Wojcik <123994953+wojcidag@users.noreply.github.com>
2023-11-13 14:04:32 +0000

    Initial commit

---
+++ RockPaperScissorsGame/RockPaperScissorsGame.py
@@ -0,0 +1,154 @@
+## Age Quod Agis
+
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&@@&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@")
+print("@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@@@&&&&&&&&&&&&&&&&&@@@@&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@@@@@&&&&&@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@&&&&&&@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@&&&@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&@@@@@@@@@@")
+print("@@@@@@@@&&&&@@&&&&&&&&&&&&&&&&&&&&&&&&&@@&@@@@@@@@@@@@@@@&&&&@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&@@@@@")
+print("@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@&&&&@@@@@@@@@@@@@@&&&&&&&@@@@@@@@&&&&&&@@@")
+print("@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@&&&&&&&&@@&&&&&&&&&&&@@@@@@@@@@@@@@&&&&@@")
+print("@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&@@&&&&@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@&&&@@")
+print("@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&@@@@&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&@@")
+print("@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&@&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&@@")
+print("@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&@&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&@@@")
+print("@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&@&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&@@@@&&&&&&&&@@@@")
+print("@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@")
+print("@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@")
+print("@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@")
+print("@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@")
+print("@@@@@@@@@@@&&&&&&&&&&@&&&&&&&&@&&@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@")
+print("@@@@@@@@@@@@&&&&&&&&&@&&&&&&&&@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@")
+print("@@@@@@@@@@@@@&&&&&&&@@&&&&&&&&&@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@")
+print("@@@@@@@@@@@@@@&&&&&&@@&&&&&&&&&&&&@@@&@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@")
+print("@@@@@@@@@@@@@@@&&&&&&@&&&&&&&&&&&&&&&@@&&&&&&&&&@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@")
+print("@@@@@@@@@@@@@@@@@&&&&@@&&&&&&&&&&&&@@@&&&&@@@@@@&&&&&&&&&&&&&&@@@@&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@")
+print("@@@@@@@@@@@@@@@@@@@&&@@&&&&&&&&&&@&&&@@@@@@&&&&&&&&&&&&&&&&&&&&&&@@@&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@")
+print("@@@@@@@@@@@@@@@@&&@@@@@@&&&&&&&&&@@@&&&&&&&&&&&&&&&&&&&@&&&&&&&&&&&@@@&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@")
+print("@@@@@@@@@@@@@@@@&&&&&@@@&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&@@@&&&&&&&&&&&&&&&&&&&&&@@@@@@@")
+print("@@@@@@@@@@@@@@@@&&&&&&@@@&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&@@@&&&&&&&&&&&&&&&&&&&&&@@@@")
+print("@@@@@@@@@@@@@@@@@&&&&&&@@&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&@@@@@@&&&&&&&&&&&&&&&&&&&&@")
+print("@@@@@@@@@@@@@@@@@@&&&&&&@&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@&&&&&&&&&&&&")
+print("@@@@@@@@@@@@@@@@@@@&&&&&@&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@&&&&&&&@@@")
+print("@@@@@@@@@@@@@@@@@@@@&&&@@&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@&&&&&&@@")
+print("@@@@@@@@@@@@@@@@@@@&&&&@&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@&&&&&@@")
+print("@@@@@@@@@@@@@@@@@@&&&&@@&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&@")
+print("@@@@@@@@@@@@@@@@@&&&@@&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&")
+print("@@@@@@@@@@@@@@@&&&@@&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@&&&@&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&@@")
+print("@@@@@@@&&&&&@&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&@@")
+print("@@@@@&&&&&&@@&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&@@")
+print()
+
+# Import Random Integer Function
+
+from random import randint
+
+# Printout of Instructions & Rules
+
+print("This is a game played between two people where in this version your")
+print("are playing against the computer.  The object of the game is to select")
+print("which of the objects, Rock, Paper or Scissors, is your guess.  The")
+print("computer also selects an object where the winner is based on the")
+print("following rules:")
+print()
+print("Rock blunts scissors (Rock wins)")
+print("Scissors cut paper (Scissors wins)")
+print("Paper wraps rock (Paper wins)")
+print()
+print("The game is a draw is both you and the computer select the same object.")
+print()
+
+# Start Guide
+
+print()
+print("Start")
+print("-" * 5)
+print()
+print("From the list below, enter the number of the object you are selecting and press ENTER")
+print()
+print("1 = Rock")
+print("2 = Paper")
+print("3 = Scissors")
+print()
+
+# Gather User Input Number
+
+user_choice = int(input("Select your number -> "))
+
+# Convert User Number to Appropriate String
+
+if user_choice == 1:
+    user_choice = str("Rock")
+    
+elif user_choice == 2:
+    user_choice = str("Paper")
+    
+else:
+    user_choice = str("Scissors")
+
+# Computer Selects Number
+
+cpu_choice = randint(1, 3)
+
+# Convert Computer Number to Appropriate String
+
+if cpu_choice == 1:
+    cpu_choice = str("Rock")
+    
+elif cpu_choice == 2:
+    cpu_choice = str("Paper")
+    
+else:
+    cpu_choice = str("Scissors")
+    
+# Determine who wins the conflict
+
+if cpu_choice == "Rock" and user_choice == "Rock":
+    print()
+    print(f"It's a Tie, Computer chose" , cpu_choice, "and you chose", user_choice)
+    
+elif cpu_choice == "Rock" and user_choice == "Paper":
+    print()
+    print(f"You win!, Computer chose" , cpu_choice , "and you chose" , user_choice)
+    print("Paper Beats Rock!")
+    
+elif cpu_choice == "Rock" and user_choice == "Scissors":
+    print()
+    print(f"You lose!, Computer chose" , cpu_choice , "and you chose" , user_choice)
+    print("Rock Beats Scissors!")
+    
+elif cpu_choice == "Paper" and user_choice == "Rock":
+    print()
+    print(f"You lose!, Computer chose" , cpu_choice , "and you chose" , user_choice)
+    print("Paper Beats Rock!")
+    
+elif cpu_choice == "Paper" and user_choice == "Paper":
+    print()
+    print(f"It's a Tie, Computer chose" , cpu_choice, "and you chose", user_choice)
+    
+elif cpu_choice == "Paper" and user_choice == "Scissors":
+    print()
+    print(f"You win!, Computer chose" , cpu_choice , "and you chose" , user_choice)
+    print("Scissors Beat Paper!")
+    
+elif cpu_choice == "Scissors" and user_choice == "Rock":
+    print()
+    print(f"You win!, Computer chose" , cpu_choice , "and you chose" , user_choice)
+    print("Rock Beats Scissors!")
+    
+elif cpu_choice == "Scissors" and user_choice == "Paper":
+    print()
+    print(f"You lose!, Computer chose" , cpu_choice , "and you chose" , user_choice)
+    print("Scissors Beat Paper!")
+    
+elif cpu_choice == "Scissors" and user_choice == "Scissors":
+    print()
+    print(f"It's a Tie, Computer chose" , cpu_choice, "and you chose", user_choice)
+    
+else:
+    print("Sorry there has been an error in this program!")
